5fb7975b5fe219af2ecdd29cd3c2b40a5d33215d
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2021-04-01 09:33:13 +0530

    Update region

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ fetch("https://ipinfo.io/json?token=c3220d5b0e7c41").then(
     console.log(jsonResponse.ip, jsonResponse.country)
 
     document.getElementById("ipaddress").innerHTML = jsonResponse.ip
+    document.getElementById("location").innerHTML = jsonResponse.region
 
   }
 )
