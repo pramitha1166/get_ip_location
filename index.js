200831a77b2a1801f7454f5cb0525ddd17341004
pramitha1166 <63492955+pramitha1166@users.noreply.github.com>
2021-04-01 09:23:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello')
+
+fetch("https://ipinfo.io/json?token=c3220d5b0e7c41").then(
+  (response) => response.json()
+).then(
+  (jsonResponse) => { 
+
+    console.log(jsonResponse)
+    console.log(jsonResponse.ip, jsonResponse.country)
+
+    document.getElementById("ipaddress").innerHTML = jsonResponse.ip
+
+  }
+)
